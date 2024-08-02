0a325594800996206b36fa209e7d1cd6c1741c84
X-spec7 <mercury.spec77@gmail.com>
2024-08-02 09:51:00 -0700

    feat: introduce clean mvvp architecture

---
+++ lib/features/auth/presentation/screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gorouter_auth/features/auth/presentation/controller/auth_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          state.when(
+              initial: () => const SizedBox(),
+              success: () => Center(
+                child: TextButton(
+                  child: const Text(
+                    'Logout',
+                  ),
+                  onPressed: () {
+
+                  },
+                ),
+              ),
+              failed: () => const SizedBox(),
+          );
+          return const SizedBox();
+        },
+      ),
+    );
+  }
+}
