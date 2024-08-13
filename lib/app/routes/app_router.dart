13de96de4836f577c67f763718423f0013958432
X-spec7 <mercury.spec77@gmail.com>
2024-08-13 10:35:11 -0700

    fix!: add bloc provider, initialize di

--- lib/app/routes/app_router.dart
+++ lib/app/routes/app_router.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gorouter_auth/app/routes/route_utils.dart';
 import 'package:gorouter_auth/app/routes/screens/not_found_page.dart';
+import 'package:gorouter_auth/features/auth/presentation/controller/auth_bloc.dart';
 import 'package:gorouter_auth/features/auth/presentation/screens/home_screen.dart';
 import 'package:gorouter_auth/features/auth/presentation/screens/login_screen.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../di/injector.dart';
 
 class AppRouter {
 
@@ -11,18 +16,26 @@ class AppRouter {
 
   static final GoRouter _router = GoRouter(
       debugLogDiagnostics: true,
-      navigatorKey: _rootNavigatorKey,
-      routes: [
-        GoRoute(
-          path: PAGES.home.appPath,
-          name: PAGES.home.appName,
-          builder: (context, state) => const HomeScreen(),
-        ),
-        GoRoute(
-          path: PAGES.login.appPath,
-          name: PAGES.login.appName,
-          builder: (context, state) => const LoginScreen(),
-        ),
+    navigatorKey: _rootNavigatorKey,
+    routes: [
+      GoRoute(
+        path: PAGES.home.screenPath,
+        name: PAGES.home.screenName,
+        builder: (context, state) =>
+            BlocProvider(
+              create: (context) => injector<AuthBloc>()..add(const IsLoggedInEvent()),
+              child: const HomeScreen(),
+            ),
+      ),
+      GoRoute(
+        path: PAGES.login.screenPath,
+        name: PAGES.login.screenName,
+        builder: (context, state) =>
+            BlocProvider(
+              create: (context) => injector<AuthBloc>(),
+              child: const LoginScreen(),
+            ),
+      ),
     ],
     errorBuilder: (context, state) => const NotFoundScreen(),
   );
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'app/di/injector.dart';
 import 'app/routes/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initDependencies();
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
